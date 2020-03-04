2535e3249d5a329f173db488099f41d5f733f196
josephat94 <josephat94@gmail.com>
2020-03-03 20:20:07 -0600

    se agregaron nuevos modelos

--- api/models/producto.model.js
+++ api/models/producto.model.js
@@ -5,7 +5,9 @@ const ProductoSchema =new Schema({
     nombre: { type: String, required: true },
     descripcion: { type: String, required: true },
     precio:{type:Number, required:true},
-    categoria: {type: mongoose.Schema.Types.ObjectId, ref: "Categoria"}
+    categoria: {type: mongoose.Schema.Types.ObjectId, ref: "Categoria"},
+    ingredientesProducto: [{type: mongoose.Schema.Types.ObjectId, ref: "IngredienteProducto"}]
+
 })
 
 module.exports = mongoose.model("Producto", ProductoSchema);
